72c323b2e9df4f2be31f59234e13ffaa533226b4
Slava <vkabarg692@gmail.com>
2026-07-29 01:56:20 +0300

    complete LCNN ASVspoof training pipeline

--- src/model/lcnn.py
+++ src/model/lcnn.py
@@ -6,6 +6,7 @@ from src.model.mfm import MaxFeatureMap
 class LCNNStem(nn.Module):
     def __init__(self) -> None:
         super().__init__()
+
         self.conv = nn.Conv2d(
             in_channels=1,
             out_channels=64,
@@ -21,4 +22,44 @@ class LCNNStem(nn.Module):
         x = self.conv(x)
         x = self.mfm(x)
         x = self.pool(x)
+        return x
+
+
+class LCNNBlock2(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.conv_4 = nn.Conv2d(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+            bias=True,
+        )
+        self.mfm_5 = MaxFeatureMap()
+        self.batch_norm_6 = nn.BatchNorm2d(32)
+
+        self.conv_7 = nn.Conv2d(
+            in_channels=32,
+            out_channels=96,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            bias=True,
+        )
+        self.mfm_8 = MaxFeatureMap()
+        self.pool_9 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.batch_norm_10 = nn.BatchNorm2d(48)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.conv_4(x)
+        x = self.mfm_5(x)
+        x = self.batch_norm_6(x)
+
+        x = self.conv_7(x)
+        x = self.mfm_8(x)
+        x = self.pool_9(x)
+        x = self.batch_norm_10(x)
+
         return x
