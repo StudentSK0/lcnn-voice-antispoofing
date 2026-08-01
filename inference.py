c4efe1e7c6afdc09ac81b7c2832fa8bcd7ca12df
Slava <vkabarg692@gmail.com>
2026-08-01 22:57:38 +0300

    document project usage and streamline entry points

--- inference.py
+++ inference.py
@@ -14,12 +14,11 @@ warnings.filterwarnings("ignore", category=UserWarning)
 @hydra.main(version_base=None, config_path="src/configs", config_name="inference")
 def main(config):
     """
-    Main script for inference. Instantiates the model, metrics, and
-    dataloaders. Runs Inferencer to calculate metrics and (or)
-    save predictions.
+    Runs Inferencer to calculate metrics and
+    save predictions
 
     Args:
-        config (DictConfig): hydra experiment config.
+        config (DictConfig): hydra experiment config
     """
     set_random_seed(config.inferencer.seed)
 
@@ -28,18 +27,13 @@ def main(config):
     else:
         device = config.inferencer.device
 
-    # setup data_loader instances
-    # batch_transforms should be put on device
     dataloaders, batch_transforms = get_dataloaders(config, device)
 
-    # build model architecture, then print to console
     model = instantiate(config.model).to(device)
     print(model)
 
-    # get metrics
     metrics = instantiate(config.metrics)
 
-    # save_path for model predictions
     save_path = ROOT_PATH / "data" / "saved" / config.inferencer.save_path
     save_path.mkdir(exist_ok=True, parents=True)
 
